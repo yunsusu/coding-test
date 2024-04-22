6ace15229f64454c885aaaed92426e5e062d939e
yunsusu <148840514+yunsusu@users.noreply.github.com>
2024-04-22 19:46:40 +0900

    [level 1] Title: 추억 점수, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,22 @@
+function solution(name, yearning, photo) {
+    var answer = [];
+    
+    photo.map((item,index)=>{
+        let box = 0;
+      for(let i = 0; i< item.length;i++){
+        // console.log(item[i])
+          for(let o = 0; o<name.length; o++){
+              // console.log(name[o])
+              if(item[i] === name[o]){
+                  box += yearning[o]
+              }
+          }
+    }
+          answer.push(box)
+    })
+    
+    
+    return answer;
+}
+
+// 7:36
