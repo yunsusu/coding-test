b0294ad01e3f95d8dc78c53e57739221fb2fdf16
yunsusu <148840514+yunsusu@users.noreply.github.com>
2024-04-23 15:44:52 +0900

    [level 2] Title: 피보나치 수, Time: 2.11 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = 0;
+    let per = 1234567;
+    
+    let num1 = 0
+    let num2 = 1
+    for(let i = 2; i<=n; i++){
+        answer = (num1+num2)%per;
+        num1 = num2;
+        num2 = answer;
+    }
+    let ans = answer
+    return ans;
+}
